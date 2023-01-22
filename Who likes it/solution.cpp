6b60b56825b548779160b21bafe8e488b880e799
Spion-Spion <105750231+Spion-Spion@users.noreply.github.com>
2023-01-22 03:59:53 +0100

    if-stat have been changed to switch-stat

--- Who likes it/solution.cpp
+++ Who likes it/solution.cpp
@@ -20,28 +20,14 @@ using namespace std;
 
 std::string likes(const std::vector<std::string>& names)
 {
-    std::string str;
-    if (names.size() == 0)
+    switch (names.size())
     {
-        str = "no one likes this";
+        case 0: return  "no one likes this";
+        case 1: return  names[0] + " likes this";
+        case 2: return  names[0] + " and " + names[1] + " like this";
+        case 3: return  names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        default: return names[0] + ", " + names[1] + " and " + std::to_string(names.size() - 2) + " others like this";
     }
-    else if (names.size() == 1)
-    {
-        str = names[0] + " likes this";
-    }
-    else if (names.size() == 2)
-    {
-        str = names[0] + " and " + names[1] + " like this";
-    }
-    else if (names.size() == 3)
-    {
-        str = names[0] + ", " + names[1] + " and " + names[2] + " like this";
-    }
-    else
-    {
-        str = names[0] + ", " + names[1] + " and " + std::to_string(names.size() - 2) + " others like this";
-    }
-    return str;
 }
 
 int main()
